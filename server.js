55fe61f14f3b7c78ebf0c64624fb3447b986c639
kairarty <kairarty@gmail.com>
2021-12-09 11:07:42 +0300

    init

---
+++ server.js
@@ -0,0 +1,10 @@
+// для установки: npm install --save express path
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(`${__dirname}/src`));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/src/index.html`));
+});
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
